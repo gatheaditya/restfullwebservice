15b4897915dd39141d5a046f0677dbaa07ef2037
gatheaditya <gatheaditya@gmail.com>
2017-11-11 21:23:49 +0530

    model added

--- src/main/java/com/rest/model/companyModel.java
+++ src/main/java/com/rest/model/companyModel.java
@@ -3,8 +3,31 @@ package com.rest.model;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public class companyModel {
 
+	private int empid;
+	private String empname;
+	private String emp_email;
+	
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmp_email() {
+		return emp_email;
+	}
+	public void setEmp_email(String emp_email) {
+		this.emp_email = emp_email;
+	}
 	
 	
 }
