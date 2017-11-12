e6e7d205672cbe3a39f2ec42c9d66418c3a5489b
gatheaditya <gatheaditya@gmail.com>
2017-11-12 15:41:05 +0530

    added

--- src/main/java/com/rest/controller/RestFullSevriveApplication.java
+++ src/main/java/com/rest/controller/RestFullSevriveApplication.java
@@ -1,9 +1,11 @@
 package com.rest.controller;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.rest")
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RestFullSevriveApplication {
 
